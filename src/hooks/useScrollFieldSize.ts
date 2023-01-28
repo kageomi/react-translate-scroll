0bcb46d278bdb0c14c9529a852aa59620b2a09a2
蔭山忠臣 <k.tadaomi@gmail.com>
2023-01-28 11:42:47 +0100

    add comments for overflow manipulation

--- src/hooks/useScrollFieldSize.ts
+++ src/hooks/useScrollFieldSize.ts
@@ -6,13 +6,17 @@ const useScrollFieldSize = (
   innerRef: React.RefObject<HTMLElement>
 ) => {
   const containerSize = useElementSize(containerRef)[0].clientSize
+
   const innerBoxElm = innerRef.current
   if (innerBoxElm != null) {
+    //scrollSize is calculated wrongly, if overflow is not auto or scroll
     innerBoxElm.style.overflowX = 'auto'
+    //to avoid showing vertical scrollbar. It effects on the width of area
     innerBoxElm.style.overflowY = 'hidden'
   }
   const innerSize = useElementSize(innerRef)[0].scrollSize
   if (innerBoxElm != null) {
+    //set overflow to default
     innerBoxElm.style.overflowX = ''
     innerBoxElm.style.overflowY = ''
   }
